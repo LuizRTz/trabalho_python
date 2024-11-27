13dcecffd9df9bac8e0e44e99974fc32382ee444
LuizRTz <luizotaviosifrade10@gmail.com>
2024-11-27 11:58:11 -0300

    V3: Template

---
+++ variaveis/var04.py
@@ -0,0 +1,9 @@
+# Solicita ao usuário a largura e a altura do retângulo
+largura = float(input("Digite a largura do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+# Calcula a área do retângulo
+area = largura * altura
+
+# Exibe o resultado
+print(f"A área do retângulo é: {area}")
